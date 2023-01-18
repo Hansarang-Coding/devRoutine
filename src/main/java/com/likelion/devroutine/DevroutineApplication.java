7499ffa041c24083b4f1e0488438e03efaef7590
DHLee <dohyunre702@gmail.com>
2023-01-18 13:09:56 +0900

    Initial commit

---
+++ src/main/java/com/likelion/devroutine/DevroutineApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.devroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevroutineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevroutineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/devroutine/DevroutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.devroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevroutineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
