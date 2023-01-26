a17234564f43ebbed822b8493e7eeda8d2048fb2
HEUNGSEOP BYEON <heungseopbyeon@gmail.com>
2023-01-26 14:59:32 +0900

    add: 소셜 로그인(#1)
    1.@EnableRedisHttpSession으로 springSessionRepositoryFilter을 빈으로 등록

--- src/main/java/com/likelion/devroutine/DevroutineApplication.java
+++ src/main/java/com/likelion/devroutine/DevroutineApplication.java
@@ -2,8 +2,10 @@ package com.likelion.devroutine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class DevroutineApplication {
 
     public static void main(String[] args) {
