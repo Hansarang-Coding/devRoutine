133ae8ce4c891629361c297a253cdcdec48159fd
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2023-01-26 13:28:13 +0900

    add: 챌린지 수정, 삭제(#2)
    1. 챌린지 수정
    2. 챌린지 삭제
    3. 기존의 ChallengeResponse -> ChallengeDto로 수정

---
+++ src/main/java/com/likelion/devroutine/challenge/dto/ChallengeDto.java
@@ -0,0 +1,51 @@
+package com.likelion.devroutine.challenge.dto;
+
+import com.likelion.devroutine.challenge.domain.Challenge;
+import com.likelion.devroutine.challenge.enumerate.AuthenticationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+public class ChallengeDto {
+    private Long id;
+    private String title;
+    private String description;
+    private AuthenticationType authenticationType;
+    private String vigibility;
+    private Long fromUserId;
+    private List<String> hashTag;
+
+    public static List<ChallengeDto> toList(List<Challenge> challenges) {
+        return challenges.stream()
+                .filter(entity->entity.getIsVigibility())
+                .map(entity -> ChallengeDto.builder()
+                        .id(entity.getId())
+                        .title(entity.getTitle())
+                        .description(entity.getDescription())
+                        .authenticationType(entity.getAuthenticationType())
+                        .fromUserId(entity.getFromUserId())
+                        .hashTag(List.of("알고리즘", "사진인증"))
+                        .vigibility(entity.getIsVigibility() ? "공개" : "비공개")
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    public static ChallengeDto toDto(Challenge challenge) {
+        return ChallengeDto.builder()
+                .id(challenge.getId())
+                .title(challenge.getTitle())
+                .description(challenge.getDescription())
+                .authenticationType(challenge.getAuthenticationType())
+                .fromUserId(challenge.getFromUserId())
+                .hashTag(List.of("알고리즘", "사진인증"))
+                .vigibility(challenge.getIsVigibility() ? "공개" : "비공개")
+                .build();
+    }
+}
