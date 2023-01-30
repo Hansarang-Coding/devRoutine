b8ef65568fed667efb4210e1ade9ab75b8c4622e
HEUNGSEOP BYEON <heungseopbyeon@gmail.com>
2023-01-30 15:54:42 +0900

    add: 댓글 예외처리 추가(#10)
    1.글쓴이가 아닌 사람이 지우려할 때 에러
    2.댓글이 이미 없는데 지우려고할 때 에러

---
+++ src/main/java/com/likelion/devroutine/comment/exception/UserUnauthorizedException.java
@@ -0,0 +1,10 @@
+package com.likelion.devroutine.comment.exception;
+
+import com.likelion.devroutine.exception.UnauthorizedException;
+
+public class UserUnauthorizedException extends UnauthorizedException {
+    private final static String MESSAGE = "해당 댓글을 삭제할 수 있는 권한이 없습니다.";
+    public UserUnauthorizedException() {
+        super(MESSAGE);
+    }
+}
