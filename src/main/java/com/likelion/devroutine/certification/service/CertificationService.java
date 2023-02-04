f42406a60c44746f668906f48c866e27e0e7784f
cmkxak <gcm466@naver.com>
2023-02-04 17:47:07 +0900

    refactor : 인증 관련 비즈니스 로직 (#15) 1. challengeId가 아닌 participationId를 활용하여 URI 구성 2. 유저 검증 메서드 분리 3. Participation을 활용하여 참가중인 챌린지 리스트가 반환되도록 구현 3. 불필요한 로직 제거

--- src/main/java/com/likelion/devroutine/certification/service/CertificationService.java
+++ src/main/java/com/likelion/devroutine/certification/service/CertificationService.java
@@ -1,14 +1,8 @@
 package com.likelion.devroutine.certification.service;
 
 import com.likelion.devroutine.certification.domain.Certification;
-import com.likelion.devroutine.certification.dto.CertificationCreateRequest;
-import com.likelion.devroutine.certification.dto.CertificationCreateResponse;
-import com.likelion.devroutine.certification.dto.CertificationFormResponse;
-import com.likelion.devroutine.certification.dto.CertificationResponse;
+import com.likelion.devroutine.certification.dto.*;
 import com.likelion.devroutine.certification.repository.CertificationRepository;
-import com.likelion.devroutine.challenge.domain.Challenge;
-import com.likelion.devroutine.challenge.exception.ChallengeNotFoundException;
-import com.likelion.devroutine.challenge.repository.ChallengeRepository;
 import com.likelion.devroutine.participant.domain.Participation;
 import com.likelion.devroutine.participant.exception.ParticipationNotFoundException;
 import com.likelion.devroutine.participant.repository.ParticipationRepository;
@@ -28,35 +22,33 @@ public class CertificationService {
 
     private final CertificationRepository certificationRepository;
     private final ParticipationRepository participantRepository;
-    private final ChallengeRepository challengeRepository;
     private final UserRepository userRepository;
 
     @Transactional
-    public CertificationCreateResponse createCertification(Long challengeId, CertificationCreateRequest request,
-                                                           String oauthId) {
-        User user = findUser(oauthId);
-        Challenge challenge = findChallenge(challengeId);
-        Certification certification = Certification.createCertification(request.getCertImage().getOriginalFilename(),
-                request.getDescription(), findParticipation(user, challenge));
+    public CertificationCreateResponse createCertification(Long participationId, CertificationCreateRequest request,
+                                                           String oauthId, String uploadImageUrl) {
+        validateUserExists(oauthId);
+        Certification certification = Certification.createCertification(uploadImageUrl,
+                request.getDescription(), findParticipation(participationId));
         Certification savedCertification = certificationRepository.save(certification);
         return CertificationCreateResponse.of(savedCertification);
     }
 
-    public CertificationFormResponse findParticipationInfo(Long challengeId, String oauthId) {
-        User user = findUser(oauthId);
-        Challenge challenge = findChallenge(challengeId);
-        Participation participation = findParticipation(user, challenge);
+    public CertificationFormResponse findCertificationFormInfo(Long participationId, String oauthId) {
+        validateUserExists(oauthId);
+        Participation participation = findParticipation(participationId);
         return CertificationFormResponse.of(participation);
     }
 
-    private Participation findParticipation(User user, Challenge challenge) {
-        return participantRepository.findByUserAndChallenge(user, challenge)
+    private Participation findParticipation(Long participationId) {
+        return participantRepository.findById(participationId)
                 .orElseThrow(ParticipationNotFoundException::new);
     }
 
-    private Challenge findChallenge(Long challengeId) {
-        return challengeRepository.findById(challengeId)
-                .orElseThrow(ChallengeNotFoundException::new);
+    public List<CertificationListResponse> findAllParticipationByUser(String oauthId) {
+        User user = findUser(oauthId);
+        List<Participation> participations = participantRepository.findAllByUserId(user.getId());
+        return CertificationListResponse.of(participations);
     }
 
     private User findUser(String oauthId) {
@@ -64,9 +56,8 @@ public class CertificationService {
                 .orElseThrow(UserNotFoundException::new);
     }
 
-    public List<CertificationResponse> findCertifications(Long challengeId, String oauthId) {
-        Participation participation = findParticipation(findUser(oauthId), findChallenge(challengeId));
-        List<Certification> certifications = certificationRepository.findByParticipationId(participation.getId());
-        return CertificationResponse.of(certifications);
+    public void validateUserExists(String oauthId) {
+        if (!userRepository.existsByOauthId(oauthId))
+            throw new UserNotFoundException();
     }
 }
