45b0b179b02498002485ca4dbd689eb24dc8aed9
cmkxak <gcm466@naver.com>
2023-02-04 17:31:07 +0900

    add : View를 위한 컨트롤러 구현 (#15) 1. uri 수정 2. 인증 생성 시, 필요한 데이터를 받아오도록 구현 3. 인증되어야 할 챌린지 리스트가 조회될 때, 필요한 데이터를 받아오도록 구현

--- src/main/java/com/likelion/devroutine/certification/controller/CertificationController.java
+++ src/main/java/com/likelion/devroutine/certification/controller/CertificationController.java
@@ -1,5 +1,9 @@
 package com.likelion.devroutine.certification.controller;
 
+import com.likelion.devroutine.certification.dto.CertificationCreateRequest;
+import com.likelion.devroutine.certification.dto.CertificationListResponse;
+import com.likelion.devroutine.certification.dto.amazons3.FileUploadResponse;
+import com.likelion.devroutine.certification.service.AmazonS3UploadService;
 import com.likelion.devroutine.certification.service.CertificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -7,19 +11,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class CertificationController {
 
     private final CertificationService certificationService;
+    private final AmazonS3UploadService s3UploadService;
 
-    @GetMapping("/{challengeId}/certificate")
-    public String certificateForm(@PathVariable("challengeId") Long challengeId,
-                                  Model model, Authentication authentication) {
-        model.addAttribute("challenge", certificationService
-                .findParticipationInfo(challengeId, authentication.getName()));
-        model.addAttribute("challengeId", challengeId);
+    @GetMapping("/certification/{participationId}")
+    public String getCertificationForm(@PathVariable("participationId") Long participationId,
+                                       Model model, Authentication authentication) {
+        model.addAttribute("participation", certificationService
+                .findParticipationFormInfo(participationId, authentication.getName()));
         return "certification/form";
+    }
+
+    @GetMapping("/certification")
+    public String getCertificationList(Authentication authentication, Model model) {
+        List<CertificationListResponse> certifications = certificationService
+                .findAllParticipationByUser(authentication.getName());
+        model.addAttribute("certifications", certifications);
+        return "certification/list";
+    }
+
+    @PostMapping("/certification/{participationId}")
+    public String createCertification(@PathVariable Long participationId,
+                                      CertificationCreateRequest request,
+                                      Authentication authentication) {
+        try {
+            FileUploadResponse certification = s3UploadService
+                    .uploadFiles(participationId, request.getCertImage(), "certificationFile");
+            certificationService.createCertification(participationId, request, authentication.getName(), certification.getUploadImageUrl());
+            return "redirect:/certification";
+        } catch (IOException e) {
+            return "error";
+        }
     }
 }
