eed450242c8d8d10e0d926ff78a92b4fdbd68345
cmkxak <gcm466@naver.com>
2023-02-05 18:29:22 +0900

    add : 인증 모음 글을 최신 날짜 순으로 내림차순 정렬하기 위한 JPQL 메서드 추가(#23)

--- src/main/java/com/likelion/devroutine/certification/repository/CertificationRepository.java
+++ src/main/java/com/likelion/devroutine/certification/repository/CertificationRepository.java
@@ -2,9 +2,13 @@ package com.likelion.devroutine.certification.repository;
 
 import com.likelion.devroutine.certification.domain.Certification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
     List<Certification> findByParticipationId(Long participationId);
+
+    @Query("select c from Certification c order by c.createdAt desc")
+    List<Certification> findAllCertification();
 }
