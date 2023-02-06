69a534cfafee16758858225eae3cf61ff373e878
cmkxak <gcm466@naver.com>
2023-02-06 11:23:28 +0900

    update : 인증 관련 dto (#23) 1. 챌린지 제목, 이름, 생성일 받아오도록 수정 2. 참가중인 챌린지를 받아오도록 수정 3. 인증 상세 조회 시 이미지와 설명 받아오도록 DTO 클래스 추가

--- src/main/java/com/likelion/devroutine/certification/dto/CertificationResponse.java
+++ src/main/java/com/likelion/devroutine/certification/dto/CertificationResponse.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,12 +14,18 @@ import java.util.stream.Collectors;
 @Getter
 public class CertificationResponse {
     private Long certificationId;
+    private String challengeTitle;
+    private String name;
+    private LocalDate createdDate;
     private String uploadImageUrl;
 
     public static List<CertificationResponse> of(List<Certification> certifications){
         return certifications.stream()
                 .map(certification -> CertificationResponse.builder()
                         .certificationId(certification.getId())
+                        .challengeTitle(certification.getParticipation().getChallenge().getTitle())
+                        .name(certification.getParticipation().getUser().getName())
+                        .createdDate(certification.getCreatedAt().toLocalDate())
                         .uploadImageUrl(certification.getUploadImageUrl())
                         .build())
                 .collect(Collectors.toList());
