4d64f3486004ed7b047c5457efd71da1350a5bdf
seop93 <110223383+seop93@users.noreply.github.com>
2023-02-08 10:06:20 +0900

    커밋

---
+++ src/main/resources/templates/static/custom.js
@@ -0,0 +1,19 @@
+633 bytes
+function updateComment() {
+    let commentBean = {
+        name:$("#commentName").val(),
+        content: $("#commentContent").val(),
+        post_num: $("#commentPostNum").val()
+    };
+    $.ajax({
+        url: "/api/v1/certification/{certificationId}/comments",
+        type: "POST",
+        data: JSON.stringify(commentBean),
+        contentType:"application/json; charset=utf-8", //body 데이터가 어떤 타입인지(MIME)
+        dataType: "json"
+    }).done(function (fragment) {
+            $('#commentTable').replaceWith(fragment);
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+}
