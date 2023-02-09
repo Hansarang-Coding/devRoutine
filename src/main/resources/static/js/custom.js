fbe65b1a774984d6548b2013f4d1ce6105ce8f00
cmkxak <gcm466@naver.com>
2023-02-09 09:25:10 +0900

    add : ajax를 위한 메서드 추가 (#23) 1. 모달 클릭 시 이미지 확대 되도록 함수 추가 2. 좋아요 처리를 위한 updateLike() 함수 추가

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,18 +1,83 @@
-function updateComment() {
-    let commentBean = {
-        name:$("#commentName").val(),
-        content: $("#commentContent").val(),
-        post_num: $("#commentPostNum").val()
+$('#comment-btn').on("click", function () {
+    let certificationId = $("#cert-id").val();
+    updateComment(certificationId);
+});
+
+$('#likeImg').on("click", function () {
+    let certificationId = $("#cert-id").val();
+    updateLike(certificationId);
+});
+
+function updateComment(certificationId) {
+    let commentDto = {
+        comment: $("#commentContent").val()
     };
     $.ajax({
-        url: "/api/v1/certification/{certificationId}/comments",
+        url: "/api/v1/certification/" + certificationId + "/comments",
         type: "POST",
-        data: JSON.stringify(commentBean),
-        contentType:"application/json; charset=utf-8", //body 데이터가 어떤 타입인지(MIME)
+        data: JSON.stringify(commentDto),
+        contentType: "application/json; charset=utf-8",
         dataType: "json"
-    }).done(function (fragment) {
-            $('#commentTable').replaceWith(fragment);
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
+
+    }).done(function (response) {
+        alert("댓글 작성을 완료하였습니다.")
+        location.href = "/certification/" + certificationId
+    }).fail(function (error) {
+        alert(JSON.stringify(error));
+    });
 }
+
+function updateLike(certificationId) {
+    let likeCnt = document.getElementById("like-cnt")
+    $.ajax({
+        url: "/api/v1/certification/" + certificationId + "/likes",
+        type: "POST",
+        data: JSON.stringify(certificationId),
+        contentType: "application/json; charset=utf-8",
+    }).done(function (response) {
+        console.log(response)
+        if (response.message === "좋아요 생성 성공") {
+            $("#likeImg").attr("src", "/assets/like_click.png");
+            $("#like-cnt").text($("#like-cnt").val() + 1)
+        } else if (response.message === "좋아요 취소") {
+            $("#likeImg").attr("src", "/assets/like_empty.png");
+            console.log($("#like-cnt").text())
+            $("#like-cnt").text($("#like-cnt").text()-1)
+        }
+    }).fail(function (error) {
+        console.log(JSON.stringify(error))
+        alert(JSON.stringify(error));
+    });
+}
+
+$(function(){
+    $(".cert-card").click(function(){
+        $(".modal").show();
+        // 해당 이미지 가겨오기
+        var imgSrc = $(this).children("img").attr("src");
+        var imgAlt = $(this).children("img").attr("alt");
+        $(".modalBox img").attr("src", imgSrc);
+        $(".modalBox img").attr("alt", imgAlt);
+
+        // 해당 이미지 텍스트 가져오기
+        var imgTit =  $(this).children("p").text();
+        $(".modalBox p").text(imgTit);
+
+        // 해당 이미지에 alt값을 가져와 제목으로
+        //$(".modalBox p").text(imgAlt);
+    });
+
+    //.modal안에 button을 클릭하면 .modal닫기
+    $(".modal button").click(function(){
+        $(".modal").hide();
+    });
+
+    //.modal밖에 클릭시 닫힘
+    $(".modal").click(function (e) {
+        if (e.target.className != "modal") {
+            return false;
+        } else {
+            $(".modal").hide();
+        }
+    });
+});
