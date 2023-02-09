c99874eafa8730703389cf16156d969395e57cd8
cmkxak <gcm466@naver.com>
2023-02-09 09:17:35 +0900

    update : 불필요한 어노테이션 제거

--- src/main/java/com/likelion/devroutine/DevroutineApplication.java
+++ src/main/java/com/likelion/devroutine/DevroutineApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
 public class DevroutineApplication {
 
     public static void main(String[] args) {
