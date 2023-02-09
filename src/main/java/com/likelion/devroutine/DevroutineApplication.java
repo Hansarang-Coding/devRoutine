a6791e3be2766c945e53333638e1078acea95b51
cmkxak <gcm466@naver.com>
2023-02-09 09:33:43 +0900

    update : 불필요한 import 제거

--- src/main/java/com/likelion/devroutine/DevroutineApplication.java
+++ src/main/java/com/likelion/devroutine/DevroutineApplication.java
@@ -2,7 +2,6 @@ package com.likelion.devroutine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 public class DevroutineApplication {
