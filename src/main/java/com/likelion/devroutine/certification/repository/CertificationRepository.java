d1c25d96eaa4be0673c2a6a6f721debd6f4b75c3
cmkxak <gcm466@naver.com>
2023-02-12 14:56:47 +0900

    add : 참가 챌린지 id로 인증 게시글을 찾는 쿼리 메소드 추가 (#34)

--- src/main/java/com/likelion/devroutine/certification/repository/CertificationRepository.java
+++ src/main/java/com/likelion/devroutine/certification/repository/CertificationRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CertificationRepository extends JpaRepository<Certification, Long> {
     List<Certification> findByParticipationIdOrderByCreatedAtDesc(Long participationId);
 
     @Query("select c from Certification c order by c.createdAt desc")
     List<Certification> findAllCertification();
+
+    List<Certification> findByParticipationId(Long participationId);
 }
