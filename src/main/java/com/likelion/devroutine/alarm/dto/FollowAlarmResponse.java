04334a151bba92187d645819a297f43c4bb5259d
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2023-02-12 23:54:05 +0900

    update : 2주 이내 알람만 가져오도록 수정(#31)
    1. 2주 이내 알람만 가져오도록 수정
    2. 알람 시간 ui에 보이도록 수정

--- src/main/java/com/likelion/devroutine/alarm/dto/FollowAlarmResponse.java
+++ src/main/java/com/likelion/devroutine/alarm/dto/FollowAlarmResponse.java
@@ -1,7 +1,10 @@
 package com.likelion.devroutine.alarm.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class FollowAlarmResponse {
@@ -10,11 +13,15 @@ public class FollowAlarmResponse {
     private String userName;
     private String userImage;
 
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy/dd/mm hh:mm:ss")
+    private LocalDateTime createdAt;
+
     @QueryProjection
-    public FollowAlarmResponse(Long alarmId, Long userId, String userName, String userImage){
+    public FollowAlarmResponse(Long alarmId, Long userId, String userName, String userImage, LocalDateTime createdAt){
         this.alarmId=alarmId;
         this.userId=userId;
         this.userName=userName;
         this.userImage=userImage;
+        this.createdAt=createdAt;
     }
 }
--- src/main/java/com/likelion/devroutine/alarm/repository/AlarmRepositoryCustomImpl.java
+++ src/main/java/com/likelion/devroutine/alarm/repository/AlarmRepositoryCustomImpl.java
@@ -8,6 +8,7 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.likelion.devroutine.alarm.domain.QAlarm.alarm;
@@ -22,7 +23,7 @@ public class AlarmRepositoryCustomImpl implements AlarmRepositoryCustom{
         return queryFactory.select(Projections.constructor(CertificationAlarmResponse.class, alarm.id, user.id, user.name, user.picture, alarm.targetId, alarm.createdAt))
                 .from(alarm)
                 .leftJoin(user).on(user.id.eq(alarm.fromUserId))
-                .where(alarm.alarmType.eq(AlarmType.NEW_LIKE_ON_CERTIFICATION), alarm.user.id.eq(userId))
+                .where(alarm.alarmType.eq(AlarmType.NEW_LIKE_ON_CERTIFICATION), alarm.user.id.eq(userId), alarm.createdAt.after(LocalDateTime.now().minusWeeks(2)))
                 .orderBy(alarm.createdAt.desc())
                 .fetch();
     }
@@ -32,17 +33,17 @@ public class AlarmRepositoryCustomImpl implements AlarmRepositoryCustom{
         return queryFactory.select(Projections.constructor(CertificationAlarmResponse.class, alarm.id, user.id, user.name, user.picture, alarm.targetId, alarm.createdAt))
                 .from(alarm)
                 .leftJoin(user).on(user.id.eq(alarm.fromUserId))
-                .where(alarm.alarmType.eq(AlarmType.NEW_COMMENT_ON_CERTIFICATION), alarm.user.id.eq(userId))
+                .where(alarm.alarmType.eq(AlarmType.NEW_COMMENT_ON_CERTIFICATION), alarm.user.id.eq(userId), alarm.createdAt.after(LocalDateTime.now().minusWeeks(2)))
                 .orderBy(alarm.createdAt.desc())
                 .fetch();
     }
 
     @Override
     public List<FollowAlarmResponse> findFollowAlarmByUser(Long userId) {
-        return queryFactory.select(Projections.constructor(FollowAlarmResponse.class, alarm.id, user.id, user.name, user.picture))
+        return queryFactory.select(Projections.constructor(FollowAlarmResponse.class, alarm.id, user.id, user.name, user.picture, alarm.createdAt))
                 .from(alarm)
                 .leftJoin(user).on(user.id.eq(alarm.fromUserId))
-                .where(alarm.alarmType.eq(AlarmType.NEW_FOLLOW), alarm.user.id.eq(userId))
+                .where(alarm.alarmType.eq(AlarmType.NEW_FOLLOW), alarm.user.id.eq(userId), alarm.createdAt.after(LocalDateTime.now().minusWeeks(2)))
                 .orderBy(alarm.createdAt.desc())
                 .fetch();
     }
