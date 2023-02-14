e2d472ab408773c4aa00ca42311f4fa696ee210e
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2023-02-14 21:30:05 +0900

    update : 비공개 챌린지 예외처리 메세지 수정(#41)

--- src/main/java/com/likelion/devroutine/challenge/exception/InaccessibleChallengeException.java
+++ src/main/java/com/likelion/devroutine/challenge/exception/InaccessibleChallengeException.java
@@ -3,7 +3,7 @@ package com.likelion.devroutine.challenge.exception;
 import com.likelion.devroutine.exception.ForbiddenException;
 
 public class InaccessibleChallengeException extends ForbiddenException {
-    private static final String MESSAGE = "해당 챌린지에 접근할 수 없습니다.";
+    private static final String MESSAGE = "비공개 챌린지입니다.";
 
     public InaccessibleChallengeException() {
         super(MESSAGE);
